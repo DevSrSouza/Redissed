fc4f6429f47f0ddc48d00d8a66f1fb123fb37733
DevSrSouza <devsrsouza@gmail.com>
2019-08-29 13:00:10 -0300

    support for JedisPool

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "br.com.devsrsouza"
-version = "1.1.0"
+version = "1.2.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/br/com/devsrsouza/redissed/clients/Jedis.kt
+++ src/main/kotlin/br/com/devsrsouza/redissed/clients/Jedis.kt
@@ -1,9 +1,12 @@
 package br.com.devsrsouza.redissed.clients
 
 import br.com.devsrsouza.redissed.RedissedCommands
+import redis.clients.jedis.Jedis
+import redis.clients.jedis.JedisPool
 import redis.clients.jedis.commands.JedisCommands
 
 val JedisCommands.redissed get() = JedisRedissedCommands(this)
+val JedisPool.redissed get() = JedisPoolRedissedCommands(this)
 
 class JedisRedissedCommands(val commands: JedisCommands) : RedissedCommands {
 
@@ -31,4 +34,33 @@ class JedisRedissedCommands(val commands: JedisCommands) : RedissedCommands {
         return commands.ttl(key)
     }
 
+}
+
+class JedisPoolRedissedCommands(val pool: JedisPool) : RedissedCommands {
+
+    val jedis: Jedis get() = pool.resource
+
+    override fun set(key: String, value: String) {
+        jedis.use { it.set(key, value) }
+    }
+
+    override fun get(key: String): String? {
+        return jedis.use { it.get(key) }
+    }
+
+    override fun contains(key: String): Boolean {
+        return jedis.use { it.exists(key) }
+    }
+
+    override fun del(key: String): Boolean {
+        return jedis.use { it.del(key) >= 1 }
+    }
+
+    override fun expire(key: String, seconds: Int): Boolean {
+        return jedis.use { it.expire(key, seconds) == 1.toLong() }
+    }
+
+    override fun ttl(key: String): Long {
+        return jedis.use { it.ttl(key) }
+    }
 }
